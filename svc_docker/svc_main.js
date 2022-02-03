2806b9c07c8e34b15526faa9b067813ae8dfc916
Bahadır Can <imbahadircan@gmail.com>
2022-02-03 16:35:34 +0000

    gcr toke version update, service update

--- svc_docker/svc_main.js
+++ svc_docker/svc_main.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from GCP Datacenters!');
+  res.send('Hello from Istanbul!');
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
